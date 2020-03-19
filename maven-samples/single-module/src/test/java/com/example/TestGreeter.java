4c191eb6b97b140f914b922b40fa8559a519240b
root <root@ip-172-31-33-7.ec2.internal>
2020-03-19 17:48:34 +0000

    polling scm

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+    public void greetShouldIncludeGreetingMessage() {
+	        String someone = "World, Thanks for correcting my grammatical errors";
+
+		    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
 }
